2eebea937859de1b05dda67655ccde2d2c6b4452
viamazoba <viamazoba@unal.edu.co>
2023-09-25 01:26:20 -0500

    chore: Installing principal libraries and Next.js 13

---
+++ src/app/page.js
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import './Home.scss'
+import React from 'react'
+import Button from '@mui/material/Button';
+
+export const Home = () => {
+  return (
+    <>
+      <div className='div'>Home</div>
+      {/* <Button variant="contained" className='button'>Hello world</Button>; */}
+    </>
+  )
+}
+
+export default Home;
+
